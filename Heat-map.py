f03978cd05cd9d6beb724186e302bf69467c7020
ElsaWang1014 <xueying0327@gmail.com>
2024-07-25 10:39:33 +0200

    Merge remote-tracking branch 'origin/master' with unrelated histories

--- Heat-map.py
+++ Heat-map.py
@@ -1,39 +1,19 @@
-<<<<<<< HEAD
 import numpy as np
 import scipy.io
 import matplotlib.pyplot as plt
-=======
-
-import numpy as np
-import scipy.io
-import matplotlib.pyplot as plt
-<<<<<<< HEAD
->>>>>>> f24c6cc052b0cd7233956b3a27714149a1c81c4d
 import correlation_Zeit_Millisecond_fertig
 import correlation_Zeit
 from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.ticker as ticker
-<<<<<<< HEAD
 from scipy import stats
 from numpy import percentile
 
-=======
-
-
-=======
-import correlation_Zeit
->>>>>>> origin/neu
->>>>>>> f24c6cc052b0cd7233956b3a27714149a1c81c4d
  
 
 # Example data
 
-<<<<<<< HEAD
 cir_realizations = correlation_Zeit_Millisecond_fertig.correlations 
 print(correlation_Zeit.correlations )  
-=======
-cir_realizations = correlation_Zeit.correlations # Dict mit round und cirs für alle Sekunden/
->>>>>>> f24c6cc052b0cd7233956b3a27714149a1c81c4d
 print(type(cir_realizations))  
 print(cir_realizations)  
 cir_realizations = np.array(cir_realizations)
@@ -44,15 +24,8 @@ print(cir_realizations.shape)
 
 def compute_cross_correlation(data):
 
-<<<<<<< HEAD
     num_realizations = len(data)
     cross_corr_matrix = np.zeros((num_realizations, num_realizations))
-=======
-    num_realizations = data.shape[0]
-
-    cross_corr_matrix = np.zeros((num_realizations, num_realizations))
-
->>>>>>> f24c6cc052b0cd7233956b3a27714149a1c81c4d
  
 
     for i in range(num_realizations):
@@ -62,7 +35,6 @@ def compute_cross_correlation(data):
         for j in range(i, num_realizations):
 
             print(j)
-<<<<<<< HEAD
             if i == j:
                 corr = 1.0
             else:
@@ -88,69 +60,21 @@ cross_corr_matrix = compute_cross_correlation(cir_realizations)
 
 print("cross_corr_matrix:", cross_corr_matrix)
 #print("Normalized matrix min and max values:", np.min(normalized_matrix), np.max(normalized_matrix))
-=======
-
-            #flattened_i = data[i].flatten()
-            #flattened_j = data[j].flatten()
-
-            # Compute cross-correlation
-            corr = np.correlate(np.array(data[i], ndmin=1), np.array(data[j], ndmin=1), mode='full')
-            norm_corr = corr / np.sqrt(np.correlate(np.array(data[i], ndmin=1), np.array(data[i], ndmin=1), mode='full')[-1] * np.correlate(np.array(data[j], ndmin=1), np.array(data[j], ndmin=1), mode='full')[-1])
-            max_corr = np.max(norm_corr)
-            
-            cross_corr_matrix[i, j] = max_corr
-
-            cross_corr_matrix[j, i] = max_corr
-
- 
-
-    return cross_corr_matrix
-
- 
-
-# Compute cross-correlation matrix
-
-cross_corr_matrix = compute_cross_correlation(cir_realizations)
-<<<<<<< HEAD
-min_corr = np.min(cross_corr_matrix)
-max_corr = np.max(cross_corr_matrix)
-normalized_matrix = (cross_corr_matrix - min_corr) / (max_corr - min_corr)
-#rank_matrix = cross_corr_matrix.argsort(axis=1).argsort(axis=1)
-#normalized_matrix = rank_matrix / (cross_corr_matrix.shape[1] - 1)
->>>>>>> f24c6cc052b0cd7233956b3a27714149a1c81c4d
 
 
 colors = [(0, 0, 1), (1, 0, 0)]  # B -> R
 n_bins = 100  # Discretizes the interpolation into bins
 cmap_name = 'blue_red'
 cm = LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bins)
-<<<<<<< HEAD
-=======
-=======
-
- 
->>>>>>> origin/neu
->>>>>>> f24c6cc052b0cd7233956b3a27714149a1c81c4d
 
 # Visualize the cross-correlation matrix
 
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(111,projection='3d')
 
-<<<<<<< HEAD
 
 X, Y = np.meshgrid(np.arange(cross_corr_matrix.shape[0]), np.arange(cross_corr_matrix.shape[1]))
 surf = ax.plot_surface(X, Y, cross_corr_matrix, cmap=cm)
-=======
-<<<<<<< HEAD
-
-#X, Y = np.meshgrid(np.arange(enhanced_matrix.shape[0]), np.arange(enhanced_matrix.shape[1]))
-
-#surf = ax.plot_surface(X, Y, enhanced_matrix, cmap=cm)
-
-X, Y = np.meshgrid(np.arange(normalized_matrix.shape[0]), np.arange(normalized_matrix.shape[1]))
-surf = ax.plot_surface(X, Y, normalized_matrix, cmap=cm)
->>>>>>> f24c6cc052b0cd7233956b3a27714149a1c81c4d
 #plt.imshow(cross_corr_matrix, cmap='hot', interpolation='nearest')
 
 fig.colorbar(surf, ax=ax, label='Normalized Cross-Correlation Coefficient')
@@ -173,23 +97,5 @@ ax.set_ylabel('Round')
 
 ax.set_zlabel('Normalized Cross-Correlation Coefficient')
 ax.set_zlim(0,1)
-<<<<<<< HEAD
-=======
-=======
-X,Y = np.meshgrid(np.arange(cir_realizations.shape[0]),np.arange(cir_realizations.shape[0]))
-
-surf = ax.plot_surface(X, Y, cross_corr_matrix, cmap='hot')
-#plt.imshow(cross_corr_matrix, cmap='hot', interpolation='nearest')
-
-fig.colorbar(surf, ax=ax, label='Cross-Correlation Coefficient')
-
-plt.title('Cross-Correlation Matrix of CIRs')
-
-ax.set_xlabel('Realization Index')
-ax.set_ylabel('Realization Index')
-ax.set_zlabel('Realization Index')
-ax.set_zlim(0.995,1)
->>>>>>> origin/neu
->>>>>>> f24c6cc052b0cd7233956b3a27714149a1c81c4d
 
 plt.show()
